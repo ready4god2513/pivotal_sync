b9a9197e9d9944160087c5cf7cf83572c9b7fd5d
Sami Makhuli <sami.makhuli@ibethel.org>
2013-04-22 16:32:50 -0700

    Adding code for membership role

--- lib/pivotal_sync/membership.rb
+++ lib/pivotal_sync/membership.rb
@@ -3,7 +3,7 @@ module PivotalSync
     include HappyMapper
     
     element :id, Integer, xpath: 'membership/id'
-    element :role, String
+    element :role, String, xpath: 'membership/role'
     element :user_id, Integer, tag: "id", deep: true
     element :name, String, deep: true
     element :email, String, deep: true
